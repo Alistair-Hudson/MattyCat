da1c9ffa43c080cb21242845a9e76dbd467895f9
AlistairHudson <alistairhudson64@gmail.com>
2023-04-08 15:15:49 +0300

    Data base for obtaining questions created

---
+++ MattyCat/Assets/Scripts/Core/QuestionDataBase.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MattyCat.Core
+{
+    public class QuestionDataBase : MonoBehaviour
+    {
+        public struct QuestionData
+        {
+            public string Question;
+            public float Answer;
+        }
+        private static Dictionary<int, List<QuestionData>> dataBase = new();
+
+        private void Start()
+        {
+            var questions = Resources.LoadAll("Questions");
+            foreach (var question in questions)
+            {
+                var q = (QuestionDataObject)question;
+                var qd = new QuestionData
+                {
+                    Question = q.Question,
+                    Answer = q.Answer
+                };
+
+                if (!dataBase.ContainsKey(q.Grade))
+                {
+                    dataBase.Add(q.Grade, new List<QuestionData>());
+                }
+                dataBase[q.Grade].Add(qd);
+            }
+        }
+
+        public static QuestionData GetQuestion(int grade)
+        {
+            return dataBase[grade][Random.Range(0, dataBase[grade].Count)];
+        }
+    }
+}
