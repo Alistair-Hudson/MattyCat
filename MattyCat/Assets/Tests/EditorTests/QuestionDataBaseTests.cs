f5cb89740a1646e0b6495c81abbdf9d72e94de66
AlistairHudson <alistairhudson64@gmail.com>
2023-04-08 16:29:27 +0300

    Added Editor Test for database

---
+++ MattyCat/Assets/Tests/EditorTests/QuestionDataBaseTests.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using MattyCat.Core;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace MattyCat.Test.Editor
+{
+    public class QuestionDataBaseTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            var obj = new GameObject();
+            var dataBase = obj.AddComponent<QuestionDataBase>();
+            dataBase.Start();
+        }
+
+        [Test]
+        public void QuestionDataBase_PopulateDataBase_Correct()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Greater(QuestionDataBase.DataBase.Count, 0, "Database has failed to populate");
+        }
+
+        [Test]
+        public void QuestionDataBase_GetQuestionFromAExistingGrade_Correct()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.NotNull(QuestionDataBase.GetQuestion(1), "Failed to get a question");
+        }
+
+        [Test]
+        public void QuestionDataBase_GetQuestionFromANonExistingGrade_Incorrect()
+        {
+            //Arrange
+
+            //Act
+            var qd = QuestionDataBase.GetQuestion(-1);
+            //Assert
+            Assert.IsTrue(string.IsNullOrEmpty(qd.Question), "Got a question");
+        }
+    }
+}
