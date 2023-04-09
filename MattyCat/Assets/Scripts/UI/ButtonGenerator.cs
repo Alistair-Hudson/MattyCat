2eb9af3e9c70988488eaebe68f59f4ef1fba2776
AlistairHudson <alistairh@oversight-ar.com>
2023-04-09 10:50:40 +0300

    Button generator created

---
+++ MattyCat/Assets/Scripts/UI/ButtonGenerator.cs
@@ -0,0 +1,39 @@
+using MattyCat.Core;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MattyCat.UI
+{
+    public class ButtonGenerator : MonoBehaviour
+    {
+        [SerializeField]
+        private Button buttonPrefab = null;
+
+        void OnEnable()
+        {
+            foreach (var key in QuestionDataBase.DataBase.Keys)
+            {
+                Button newButton = Instantiate(buttonPrefab, transform);
+                newButton.GetComponentInChildren<TMP_Text>().text = key.ToString();
+                newButton.onClick.AddListener(delegate { 
+                    SystemInformation.Grade = key; 
+                    Debug.Log($"Grade selected: {key}");
+                    SceneManager.LoadScene("MainGamePlayScene");
+                });
+            }
+        }
+
+        private void OnDisable()
+        {
+            var buttons = GetComponentsInChildren<Button>();
+            foreach(var button in buttons)
+            {
+                Destroy(button.gameObject);
+            }
+        }
+    }
+}
